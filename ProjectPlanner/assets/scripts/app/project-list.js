387c1fe9fe91fc2ce9244cac37807a3c249b9d78
Dement057 <Dement057@bitbucket.org>
2021-03-11 01:42:10 +0300

    NpmEslint

--- ProjectPlanner/assets/scripts/app/project-list.js
+++ ProjectPlanner/assets/scripts/app/project-list.js
@@ -1,64 +1,68 @@
-import { ProjectItem as PrjItem } from './project-item.js';
-import * as DOMH from '../utility/dom-helper.js';
+import { ProjectItem as PrjItem } from "./project-item.js";
+import * as DOMH from "../utility/dom-helper.js";
+
+// console.log(this);
 
 export class ProjectList {
-    projects = [];
-    constructor(type) {
-      this.type = type;
-      const prjItems = document.querySelectorAll(`#${this.type}-projects li`);
-      for (const prjItem of prjItems) {
-        this.projects.push(new PrjItem(prjItem.id, this.switchProject.bind(this), this.type));
-      }
-      console.log(this.projects);
-      this.connectDroppable();
+  constructor(type) {
+    this.projects = [];
+    this.type = type;
+    const prjItems = document.querySelectorAll(`#${this.type}-projects li`);
+
+    for (const prjItem of prjItems) {
+      this.projects.push(new PrjItem(prjItem.id, this.switchProject.bind(this), this.type));
     }
-  
-    connectDroppable() {
-      const list = document.querySelector(`#${this.type}-projects ul`);
-      list.addEventListener("dragenter", (event) => {
-        if (event.dataTransfer.types[0] === "text/plain") {
-          list.parentElement.classList.add('droppable')
-          event.preventDefault();
-        }
-      });
-      list.addEventListener("dragover", (event) => {
-        if (event.dataTransfer.types[0] === "text/plain") {
-          event.preventDefault();
-        }
-      });
-      list.addEventListener('dragleave', event => {
-        if (event.relatedTarget.closest(`#${this.type}-projects ul`) !== list){
-          list.parentElement.classList.remove('droppable');
-        }
-      })
-  
-      list.addEventListener('drop', event => {
-        const prjId = event.dataTransfer.getData('text/plain');
-        if (this.projects.find(prjItem=> prjId === prjItem.id)){
-          return;
-        }
-        document.getElementById(prjId).querySelector('button:last-of-type').click();
-        list.parentElement.classList.remove('droppable');
-        event.preventDefault();
-      })
-    }
-  
-    setSwitchHandlerFunction(switchHandlerFunction) {
-      this.switchHandler = switchHandlerFunction;
-    }
-  
-    addProject(project) {
-      // console.log(project)
-      this.projects.push(project);
-      DOMH.moveElement(project.id, `#${this.type}-projects ul`);
-      project.update(this.switchProject.bind(this), this.type);
-    }
-  
-    switchProject(projectId) {
-      // const projectIndex = this.projects.findIndex(p => p.id === projectId)
-      // this.projects.splice(projectIndex,1)
-      console.log(projectId);
-      this.switchHandler(this.projects.find((p) => p.id === projectId));
-      this.projects = this.projects.filter((p) => p.id !== projectId);
-    }
+    console.log(this.projects);
+    this.connectDroppable();
   }
+
+  connectDroppable() {
+    console.log(globalThis);
+    const list = document.querySelector(`#${this.type}-projects ul`);
+    list.addEventListener("dragenter", (event) => {
+      if (event.dataTransfer.types[0] === "text/plain") {
+        list.parentElement.classList.add("droppable");
+        event.preventDefault();
+      }
+    });
+    list.addEventListener("dragover", (event) => {
+      if (event.dataTransfer.types[0] === "text/plain") {
+        event.preventDefault();
+      }
+    });
+    list.addEventListener("dragleave", (event) => {
+      if (event.relatedTarget.closest(`#${this.type}-projects ul`) !== list) {
+        list.parentElement.classList.remove("droppable");
+      }
+    });
+
+    list.addEventListener("drop", (event) => {
+      const prjId = event.dataTransfer.getData("text/plain");
+      if (this.projects.find((prjItem) => prjId === prjItem.id)) {
+        return;
+      }
+      document.getElementById(prjId).querySelector("button:last-of-type").click();
+      list.parentElement.classList.remove("droppable");
+      event.preventDefault();
+    });
+  }
+
+  setSwitchHandlerFunction(switchHandlerFunction) {
+    this.switchHandler = switchHandlerFunction;
+  }
+
+  addProject(project) {
+    // console.log(project)
+    this.projects.push(project);
+    DOMH.moveElement(project.id, `#${this.type}-projects ul`);
+    project.update(this.switchProject.bind(this), this.type);
+  }
+
+  switchProject(projectId) {
+    // const projectIndex = this.projects.findIndex(p => p.id === projectId)
+    // this.projects.splice(projectIndex,1)
+    console.log(projectId);
+    this.switchHandler(this.projects.find((p) => p.id === projectId));
+    this.projects = this.projects.filter((p) => p.id !== projectId);
+  }
+}
--- ProjectPlanner/assets/scripts/main.js
+++ ProjectPlanner/assets/scripts/main.js
@@ -1,4 +1,6 @@
+// globalThis.DEFAULT_VALUE = 'VOVA!!';
 import { ProjectList } from './app/project-list.js';
+
 
 class App {
   static init() {
@@ -6,51 +8,51 @@ class App {
     const finishedProjectList = new ProjectList("finished");
     activeProjectList.setSwitchHandlerFunction(finishedProjectList.addProject.bind(finishedProjectList));
     finishedProjectList.setSwitchHandlerFunction(activeProjectList.addProject.bind(activeProjectList));
-  };
-    //   const timerId = setTimeout(()=>{this.startAnalitycs()}, 2000)
-    //   document.getElementById("stop-analytics").addEventListener('click',()=>{
+  }
+  //   const timerId = setTimeout(()=>{this.startAnalitycs()}, 2000)
+  //   document.getElementById("stop-analytics").addEventListener('click',()=>{
     //     clearTimeout(timerId);
     //   });
     // }
-
+    
     static startAnalitycs() {
       const analyticScript = document.createElement('script');
       analyticScript.src = 'assets/scripts/utility/analytics.js';
       analyticScript.defer = true;
       document.head.append(analyticScript);
+    }
   }
-}
-
-App.init();
-
-// function logPerson(){
-//   console.log(`Person: ${this.name}, ${this.age}, ${this.job}`)
-// };
-
-// const person1 = {name:'Макс', age:12,job:'Problem creator'}
-// const person2 = {name:'Александр', age:33,job:'Problem solver'}
-
-// const firstPair = {firstNumb:2, secondNumb:33}
-// const secPair = {firstNumb:6, secondNumb:44}
-// const thirdPair = {firstNumb:12326, secondNumb:4}
-
-// function multpler(){
-//   console.log(this.firstNumb * this.secondNumb);
-// }
-
-// function adder(){
-//   console.log(this.firstNumb + this.secondNumb);
-// }
-
-// function substricter(){
-//   console.log(this.firstNumb - this.secondNumb);
-// }
-
-// function divider(){
-//   console.log(this.firstNumb / this.secondNumb);
-// }
-
-// function binder(context, fn){
+  
+  App.init();
+  
+  // function logPerson(){
+    //   console.log(`Person: ${this.name}, ${this.age}, ${this.job}`)
+    // };
+    
+    // const person1 = {name:'Макс', age:12,job:'Problem creator'}
+    // const person2 = {name:'Александр', age:33,job:'Problem solver'}
+    
+    // const firstPair = {firstNumb:2, secondNumb:33}
+    // const secPair = {firstNumb:6, secondNumb:44}
+    // const thirdPair = {firstNumb:12326, secondNumb:4}
+    
+    // function multpler(){
+      //   console.log(this.firstNumb * this.secondNumb);
+      // }
+      
+      // function adder(){
+        //   console.log(this.firstNumb + this.secondNumb);
+        // }
+        
+        // function substricter(){
+          //   console.log(this.firstNumb - this.secondNumb);
+          // }
+          
+          // function divider(){
+            //   console.log(this.firstNumb / this.secondNumb);
+            // }
+            
+            // function binder(context, fn){
 //   context.fn = fn;
 //   return context.fn();
 // }
